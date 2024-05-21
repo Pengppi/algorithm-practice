24b38b830f1f15ddce080d02332272387cf06f04
西山对寂寥 <1781287641@qq.com>
2024-05-21 15:08:28 +0800

    feat: 2024/5/21

---
+++ leetcode/src/main/python/leetcode/editor/cn/找出最大的可达成数字findTheMaximumAchievableNumber.py
@@ -0,0 +1,13 @@
+"""
+Title: 2769.找出最大的可达成数字
+Title Slug: findTheMaximumAchievableNumber
+Author: Neo
+Date: 2024-05-21 15:07:23
+"""
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + t * 2
+# leetcode submit region end(Prohibit modification and deletion)
